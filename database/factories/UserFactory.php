3f6e7192a74f67a8ab8e982f915c984ceab6a527
nikolas <nikola.stojilkovic6@gmail.com>
2022-12-14 00:23:53 +0100

    user model modified

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
@@ -14,11 +15,12 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $username = $this->faker->unique()->userName();
         return [
-            'name' => $this->faker->name(),
+            'username' => $username,
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make($username . '123'), // password
             'remember_token' => Str::random(10),
         ];
     }
