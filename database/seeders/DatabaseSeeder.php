fa4ff439586e7c0b1e0fc3d43e813f900d7b91c3
nikolas <nikola.stojilkovic6@gmail.com>
2022-12-14 02:50:53 +0100

    seeding done properly so everything is connected

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,7 @@ use App\Models\Genre;
 use App\Models\Movie;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -24,7 +25,14 @@ class DatabaseSeeder extends Seeder
         // User::factory(1)->create();
         // Genre::factory(1)->create();
         // Movie::factory(5)->create();
+        // Comment::factory(3)->create();
 
-        Comment::factory(3)->create();
+        $user = User::create(['username' => 'nikolaS', 'password' => Hash::make('nikolaS123'), 'email' => 'nikola@gmail.com']);
+        $genre = Genre::create(['genre_name' => 'comedy']);
+
+        $movies = Movie::factory(2)->create(['user_id' => $user->id, "genre_id" => $genre->id]);
+
+        Comment::factory(7)->create(['movie_id' => $movies[0]->id]);
+        Comment::factory(10)->create(['movie_id' => $movies[1]->id]);
     }
 }
